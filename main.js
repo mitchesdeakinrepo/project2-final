4cb4aca9e794232124aea7966d3b54fe622901ed
mitchesdeakinrepo <132981771+mitchesdeakinrepo@users.noreply.github.com>
2023-09-13 19:53:16 +1000

    js editsd

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import ToDoApp from "./todoapp.vue";
 
 const app = createApp(App);
 app.mount("#app");
-
-const tdapp = createApp(ToDoApp);
-tdapp.mount("#todoapp");
